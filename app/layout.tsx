2a164423e820153a661a0de08bb274d6926dd39d
Maria Llenas <mallecat12@gmail.com>
2024-03-15 18:23:59 +0100

    Creation of general folder structure + install main dependencies

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import "./globals.css";
+import * as React from "react";
+import Navbar from "@/app/ui/Navbar";
+import { montserrat } from "@/app/ui/fonts";
+import { Footer } from "@/app/ui/Footer";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={`${montserrat.className} antialiased flex flex-col min-h-screen`}
+      >
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
