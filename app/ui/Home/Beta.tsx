9d54f2d666c72c4b548b13500bfc70e838309beb
Maria Llenas <mallecat12@gmail.com>
2024-03-17 13:12:05 +0100

    Beta component from home page layout

--- app/ui/Home/Beta.tsx
+++ app/ui/Home/Beta.tsx
@@ -1,13 +1,23 @@
 import Button from "../Button";
 export const Beta = () => {
   return (
-    <div className="h-72 flex flex-col justify-center text-center gap-4 text-white bg-cover bg-no-repeat bg-[url('/images/home/upload-background-small.png')] md:bg-[url('/images/home/upload-background.png')]">
-      <h2 className="text-3xl font-semibold">UPLOAD YOUR BETA</h2>
-      <p>
+    <div
+      className="relative text-center text-white flex flex-col justify-center gap-4 py-16 px-4 lg:p-40 bg-no-repeat bg-cover bg-right md:bg-center"
+      style={{
+        backgroundImage: "url('/images/home/upload-background-small.png')",
+      }}
+    >
+      <div className="absolute inset-0 bg-black opacity-20"></div>
+
+      <h3 className="z-10  text-4xl font-bold relative">UPLOAD YOUR BETA</h3>
+      <h3 className="z-10 font-semibold  relative">
         Do you want to help us in our journey? Upload your beta and contribute
         to the project.
-      </p>
-      <Button>UPLOADER</Button>
+      </h3>
+
+      <button className="bg-yellow-400 z-10 text-neutral-700 font-semibold rounded w-fit px-4 py-2 mx-auto">
+        UPLOADER
+      </button>
     </div>
   );
 };
