97d97a13bbb79b69bc9455d3c95c1255ce8652b6
Maria Llenas <mallecat12@gmail.com>
2024-03-26 20:46:54 +0100

    Add logic to [areaName]/guides page

--- app/areas/[areaName]/guides/page.tsx
+++ app/areas/[areaName]/guides/page.tsx
@@ -1,11 +1,44 @@
 import { CardFlip } from "@/app/ui/Areas/CardFlip";
-export default function GuidesPage() {
+import zoneData from "@/app/lib/data/zoneData.json";
+import { IArea } from "@/app/lib/types";
+import FeatherIcon from "feather-icons-react";
+
+export async function generateStaticParams() {
+  const areaNames = zoneData.items.map((area: IArea) =>
+    decodeURIComponent(area.name)
+  );
+  return areaNames.map((name) => ({ areaName: name }));
+}
+
+export default function GuidesPage({
+  params,
+}: {
+  params: { areaName: string };
+}) {
+  const currentArea = decodeURIComponent(params.areaName);
+  const currentAreaData = zoneData.items.find(
+    (zone: IArea) => zone.name == currentArea
+  );
+
+  const links = currentAreaData?.links.map((link) => {
+    return (
+      <a
+        key={link.link}
+        href={link.link}
+        target="_blank"
+        className="flex gap-2 border rounded p-2 mx-8 bg-neutral-200 font-semibold"
+      >
+        <FeatherIcon icon="book" className="text-neutral-600" /> {link.name}
+      </a>
+    );
+  });
+
   return (
-    <div className="py-4">
+    <div className="p-8 flex flex-col gap-8">
       {/* Guides Images: */}
-      <div className="">
-        <CardFlip />
-      </div>
+      <CardFlip data={currentAreaData} />
+      {/* LINKS: */}
+      <div className="flex flex-col gap-4">{links}</div>
     </div>
   );
 }
--- app/ui/Areas/CardFlip.tsx
+++ app/ui/Areas/CardFlip.tsx
@@ -2,14 +2,17 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
 import React from "react";
-import styles from "@/app/ui/Areas/cardflip.module.css";
+import styles from "./cardFlip.module.css";
+import { IArea } from "@/app/lib/types";
 
 export const Card = ({
   imageUrl,
   title,
+  link,
 }: {
   imageUrl: string;
   title: string;
+  link: string;
 }) => {
   const [isFlipped, setIsFlipped] = useState(false);
   const [isAnimating, setIsAnimating] = useState(false);
@@ -20,51 +23,53 @@ export const Card = ({
       setIsAnimating(!isAnimating);
     }
   }
-  return (
-    <div className="flex items-center justify-center">
-      <div
-        className="flip-card w-[120px] h-[180px] rounded-md"
-        onClick={handleFlip}
-      >
-        <motion.div
-          className="flip-card-inner w-[100%] h-[100%]"
-          initial={false}
-          animate={{ rotateY: isFlipped ? 180 : 360 }}
-          transition={{ duration: 0.2, animationDirection: "normal" }}
-          onAnimationComplete={() => setIsAnimating(false)}
-        >
-          <div className="flip-card-front w-[100%] h-[100%] bg-no-repeat bg-contain  text-white rounded-lg p-4 bg-[url('https://m.media-amazon.com/images/I/81UJX3rkGML._AC_UY218_.jpg')]"></div>
 
-          <div className="flip-card-back w-[100%] h-[100%] bg-cover border-[1px] text-center rounded-lg p-4 bg-neutral-300 flex flex-col justify-center gap-4">
-            <h4 className="font-semibold text-xs ">Boulder En España Guide</h4>
-            <a
-              className="bg-yellow-400 px-4 p-2 w-fit text-xs mx-auto rounded"
-              href="https://amzn.to/3UizeG0"
-              target="_blank"
-            >
-              BUY
-            </a>
-          </div>
-        </motion.div>
-      </div>
+  const imageThumbnail = imageUrl || "/images/example.jpeg";
+
+  return (
+    <div
+      className={`${styles.flipCard} w-[140px] h-[200px] rounded-md mx-auto`}
+      onClick={handleFlip}
+    >
+      <motion.div
+        className={`${styles.flipCardInner} w-[100%] h-[100%] `}
+        initial={false}
+        animate={{ rotateY: isFlipped ? 180 : 360 }}
+        transition={{ duration: 0.2, animationDirection: "normal" }}
+        onAnimationComplete={() => setIsAnimating(false)}
+      >
+        <img
+          className={`${styles.flipCardFront} w-full h-full text-white rounded-lg `}
+          src={imageThumbnail}
+        ></img>
+
+        <div
+          className={`${styles.flipCardBack} w-[100%] h-[100%] bg-cover border-[1px] text-center rounded-lg p-4 bg-neutral-300 flex flex-col justify-center gap-4`}
+        >
+          <h4 className="font-semibold text-xs ">{title}</h4>
+          <a
+            className="bg-yellow-400 px-4 p-2 w-fit text-xs mx-auto rounded"
+            href={link}
+            target="_blank"
+          >
+            BUY
+          </a>
+        </div>
+      </motion.div>
     </div>
   );
 };
 
-export const CardFlip = () => {
-  const cardData = [
-    {
-      imageUrl:
-        "https://m.media-amazon.com/images/I/81UJX3rkGML._AC_UY218_.jpg",
-      title: "Boulder En España Guide",
-    },
-    { imageUrl: "https://example.com/your-image-url.jpg", title: "Your Title" },
-  ];
-
+export const CardFlip = ({ data }: { data: IArea | undefined }) => {
   return (
-    <div className="flex items-center justify-center gap-4">
-      {cardData.map((card, index) => (
-        <Card key={index} imageUrl={card.imageUrl} title={card.title} />
+    <div className="flex flex-wrap gap-8">
+      {data?.guides.map((guide, index) => (
+        <Card
+          key={index}
+          imageUrl={guide.thumbnail}
+          title={guide.name}
+          link={guide.link[0]}
+        />
       ))}
     </div>
   );
