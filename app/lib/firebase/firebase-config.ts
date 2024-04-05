e50344de3a64cb2a1cdf7fb7fd6969779d67e394
Maria Llenas <mallecat12@gmail.com>
2024-04-05 10:44:43 +0200

    Changes in firebase config

--- app/lib/firebase/firebase-config.ts
+++ app/lib/firebase/firebase-config.ts
@@ -6,23 +6,12 @@ import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+const firebaseConfigString = process.env.NEXT_PUBLIC_FIREBASE_CONFIG
+if (!firebaseConfigString) {
+    throw new Error("Firebase config not found in environment variables.");
+}
 
-const firebaseApiKey = process.env.NEXT_PUBLIC_FIREBASE_API_KEY;
-const firebaseAuthDomain = process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN;
-const firebaseProjectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
-const firebaseStorageBucket = process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET;
-const firebaseMessagingSenderId = process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID;
-const firebaseAppId = process.env.NEXT_PUBLIC_FIREBASE_APP_ID;
-
-const firebaseConfig = {
-    apiKey: firebaseApiKey,
-    authDomain: firebaseAuthDomain,
-    projectId: firebaseProjectId,
-    storageBucket: firebaseStorageBucket,
-    messagingSenderId: firebaseMessagingSenderId,
-    appId: firebaseAppId,
-};
-
+const firebaseConfig = JSON.parse(firebaseConfigString);
 // Initialize Firebase
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
 const db = getFirestore(app)
